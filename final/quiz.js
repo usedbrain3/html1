d1e8e5d666520331dd81f04b0d75ceba9d8a4fce
cweb10102021 <90846036+cweb10102021@users.noreply.github.com>
2021-12-07 07:34:25 -0600

    Create quiz.js

---
+++ final/quiz.js
@@ -0,0 +1,363 @@
+var questions = [
+    {
+        "question": "Media Queries allow CSS styling to change based on browser conditions", 
+        "correctAnswer" : "True"
+    },
+    {
+        "question": "Flexible Structure does not let content on a website adapt to the size of the browser window", 
+        "correctAnswer" : "False"
+    },
+    {
+        "question": "Complementary colors are next to each other on the color wheel", 
+        "correctAnswer" : "False"
+    },
+    {
+        "question": "JQuery is a JavaScript library", 
+        "correctAnswer" : "True"
+    },
+    {
+        "question": "Analogous colors are opposite each other on the color wheel", 
+        "correctAnswer" : "False"
+    },
+    {
+        "question": "Input element tags allow users to input data", 
+        "correctAnswer" : "True"
+    },
+    {
+        "question": "The CSS property 'float' can change the position of a specific element on a webpage", 
+        "correctAnswer" : "True"
+    },
+    {
+        "question": "Normal flow is the default positioning of elements in HTML", 
+        "correctAnswer" : "True"
+    },
+    {
+        "question": "An valid example of a pseudo-class is 'h1:hover'", 
+        "correctAnswer" : "True"
+    },
+    {
+        "question": ".innerHTML changes an HTML element's name", 
+        "correctAnswer" : "False"
+    },
+    {
+        "question": "Server-side scripting is scripts that are written to run on the server side rather than on the frontend", 
+        "correctAnswer" : "True"
+    },
+    {
+        "question": "Operator precedence determines determines the order in which expressions are validated", 
+        "correctAnswer" : "True"
+    },
+    {
+        "question": "Progressive Enhancement is a JavaScript command that enhances your code for you", 
+        "correctAnswer" : "False"
+    },
+    {
+        "question": "Arrays can only contain one object", 
+        "correctAnswer" : "False"
+    },
+    {
+        "question": "While statements will repeat a function as long as the conditions stated are still met", 
+        "correctAnswer" : "True"
+    },
+    {
+        "question": "Attributes are required on HTML elements", 
+        "correctAnswer" : "False"
+    },
+    {
+        "question": "A valid example of an argument is 'getElementById('h1'), where h1 is the argument", 
+        "correctAnswer" : "True"
+    },
+    {
+        "question": "Client-side scripting is scripts written in a language made to run on the server side, or the backend", 
+        "correctAnswer" : "False"
+    },
+    {
+        "question": "a 'for' statement is an example of a looping statement", 
+        "correctAnswer" : "True"
+    },
+    {
+        "question": "JavaScript is not case-sensitive", 
+        "correctAnswer" : "False"
+    },
+    {
+        "question": "'container' is a valid identifier, or ID", 
+        "correctAnswer" : "True"
+    },
+    {
+        "question": "Initializing is giving a value to a variable", 
+        "correctAnswer" : "True"
+    },
+    {
+        "question": "if statements execute at least once", 
+        "correctAnswer" : "False"
+    },
+    {
+        "question": "'var object = document.getElementByID('objectID')' is a valid example of defining an object", 
+        "correctAnswer" : "True"
+    },
+    {
+        "question": "&#60;script&#62; tags are used to define JavaScript text", 
+        "correctAnswer" : "True"
+    },
+    {
+        "question": "getElementByID returns all instances of a certain element", 
+        "correctAnswer" : "False"
+    },
+    {
+        "question": "++ is a concatenation operator", 
+        "correctAnswer" : "False"
+    },
+    {
+        "question": "'' can be used to create an emopty string", 
+        "correctAnswer" : "True"
+    },
+    {
+        "question": "&#60;link href='script.js'&#62; is a valid way to import a JavaScript document", 
+        "correctAnswer" : "False"
+    },
+    {
+        "question": "/* this is a CSS comment */", 
+        "correctAnswer" : "True"
+    },
+    {
+        "question": "'onclick' is a valid example of an event handler", 
+        "correctAnswer" : "True"
+    },
+    {
+        "question": "++ is not a unary operator", 
+        "correctAnswer" : "False"
+    },
+    {
+        "question": "'text-size' can be used to make a font bold", 
+        "correctAnswer" : "False"
+    },
+    {
+        "question": "A global variable is defined outside of function", 
+        "correctAnswer" : "True"
+    },
+    {
+        "question": "'padding: 5px 2px 3px 1px;' targets each side of an element clockwise starting from the top", 
+        "correctAnswer" : "True"
+    },
+    {
+        "question": "candy[3]='Sour Patch Kids'; adds 'Sour Patch Kids' as the third element in the candy array", 
+        "correctAnswer" : "False"
+    },
+    {
+        "question": ".box is used to select an element with the ID 'box'", 
+        "correctAnswer" : "False"
+    },
+    {
+        "question": ".container is used to select an element with the class 'container'", 
+        "correctAnswer" : "True"
+    },
+    {
+        "question": "&#60;image href='image.png' alt='image'&#62; is a valid way to import an image into HTML", 
+        "correctAnswer" : "False"
+    },
+    {
+        "question": "&#60;!-- This is an HTML comment --&#62;", 
+        "correctAnswer" : "True"
+    },
+    {
+        "question": "&#60;!-- This is a JavaScript comment --&#62;", 
+        "correctAnswer" : "False"
+    },
+    {
+        "question" : "window.alert() will make the browser play an alert sound",
+        "correctAnswer" : "False"
+    },
+    {
+        "question" : "for statements allow you to initialize a counter that changes its value each time the statements are ran",
+        "correctAnswer" : "True"
+    },
+    {
+        "question" : "indexes returns the number of elements in an array",
+        "correctAnswer" : "False"
+    },
+    {
+        "question" : "true and false are Boolean values",
+        "correctAnswer" : "True"
+    },
+    { 
+        "question" : "Functions can contain more than one statement that can be executes as a single unit",
+        "correctAnswer" : "True"
+    },
+    {
+        "question" : "Function parameters are placed within curly brackets",
+        "correctAnswer" : "False"
+    },
+    {
+        "question" : "A local variable is defined outside of a function",
+        "correctAnswer" : "False"
+    },
+    {
+        "question" : "JavaScript is a strongly typed coding language",
+        "correctAnswer" : "False"
+    },
+    {
+        "question" : "The content of the HTML element with the ID 'box' can be targeted using document.getElementByID('box').innerHTML;",
+        "correctAnswer" : "True"
+    },
+    {
+        "question" : "JavaScript functions are written as function = functionExample()",
+        "correctAnswer" : "False"
+    },
+    {
+        "question" : "if(x == 20) is a valid example of an if statement",
+        "correctAnswer" : "True"
+    },
+    {
+        "question" : "if(x != 20) is the correct way to write x is NOT equal to 20",
+        "correctAnswer" : "True"
+    },
+    {
+        "question" : "A for loop starts with for(x == 5; y++)",
+        "correctAnswer" : "False"
+    },
+    {
+        "question" : "var candy = ['Snickers', 'Nerds', 'M&Ms'] is the correct way to write a JavaScript array",
+        "correctAnswer" : "False"
+    },
+    {
+        "question" : "JavaScript is not the same as Java",
+        "correctAnswer" : "True"
+    },
+    {
+        "question" : "'var Candy' is a valid way of declaring a JavaScript variable",
+        "correctAnswer" : "True"
+    },
+    {
+        "question" : "JavaScript is not case-sensitive",
+        "correctAnswer" : "False"
+    },
+    {
+        "question" : "'.addEventListener('click', function()) adds an 'onclick' event listener to an element",
+        "correctAnswer" : "True"
+    },
+    {
+        "question" : "A function can only be called once",
+        "correctAnswer" : "False"
+    },
+    {
+        "question" : "getElementsByArray can retrieve a JavaScript arary",
+        "correctAnswer" : "False"
+    }
+    
+];
+
+// Load first question on page laod
+let currentQuestion = 0;
+
+var wrongCount = 0, rightCount = 0;
+
+// Sets total amount of questions to total in questions array
+const totalQuestions = questions.length;
+
+// Target Question card to be populated by question in question array
+const card = document.querySelector('#card');
+const answerButton = document.querySelector('#truefalse');
+const nextButton = document.querySelector('#next');
+
+// Populate Question card with Question
+function generateQuestions(index) {
+    console.log(index);
+    // Push Question text to Card
+    card.innerHTML = questions[index].question;
+}
+
+function loadNextQuestion() {
+    currentQuestion++;
+    answerButton.style.display="flex";
+    nextButton.style.display="none";
+    card.style.color = "white";
+    card.style.transition = "transform 0s ease-in";
+
+    if(currentQuestion == totalQuestions - 1) {
+        nextButton.textContent = 'Show Results';
+    }
+
+    if(currentQuestion == totalQuestions) {
+        showQuestions();
+    }
+    
+    else{
+        generateQuestions(currentQuestion);
+    }
+} 
+
+// If the True is chosen:
+function answerTrue(){
+    if (questions[currentQuestion].correctAnswer == 'True'){
+        card.innerHTML =  "<p><span id='correct_text' class='answer_text'>Correct!</span><br>" + questions[currentQuestion].question + "<br>is " + questions[currentQuestion].correctAnswer + "</p>";
+        var correctText = document.getElementById("correct_text");
+        card.style.transition = "all .2s";
+        correctText.style.color = "#55AA44";
+
+        rightCount+=1;
+        document.getElementById('correct_talley').innerHTML = rightCount;
+    }
+
+    else{
+        card.innerHTML = "<p><span id='incorrect_text' class='answer_text'>Incorrect :(</span><br>" + questions[currentQuestion].question + "<br>is " + questions[currentQuestion].correctAnswer + "</p>";
+        var incorrectText = document.getElementById("incorrect_text");
+        incorrectText.style.color = "#993311";
+        card.style.transition = "all .2s";
+
+        wrongCount +=1;
+        document.getElementById('wrong_talley').innerHTML = wrongCount;
+    }
+
+    answerButton.style.display="none";
+    nextButton.style.display="block";
+}
+
+function answerFalse(){
+    if (questions[currentQuestion].correctAnswer == 'False'){
+        card.innerHTML =  "<p><span id='correct_text' class='answer_text'>Correct!</span><br>" + questions[currentQuestion].question + "<br>is " + questions[currentQuestion].correctAnswer + "</p>";
+        var correctText = document.getElementById("correct_text");
+        card.style.transition = "all .2s";
+        correctText.style.color = "#55AA44";
+
+        rightCount+=1;
+        document.getElementById('correct_talley').innerHTML = rightCount;
+    }
+
+    else{
+        card.innerHTML = "<p><span id='incorrect_text' class='answer_text'>Incorrect :(</span><br>" + questions[currentQuestion].question + "<br>is " + questions[currentQuestion].correctAnswer + "</p>";
+        var incorrectText = document.getElementById("incorrect_text");
+        incorrectText.style.color = "#993311";
+        card.style.transition = "all .2s";
+
+        wrongCount +=1;
+        document.getElementById('wrong_talley').innerHTML = wrongCount;
+    }
+
+    answerButton.style.display="none";
+    nextButton.style.display="block";
+}
+
+// Add onclick to true and false buttons
+var trueButton = document.getElementById('trueButton');
+trueButton.addEventListener('click', answerTrue);
+var falseButton = document.getElementById('falseButton');
+falseButton.addEventListener('click', answerFalse);
+
+// On page load, loads first question
+generateQuestions(currentQuestion);
+nextButton.addEventListener('click', loadNextQuestion);
+
+// Populate Questions Page
+var questionText = document.getElementsByClassName('question');
+var answerText = document.getElementsByClassName('question_answer');
+
+var questionContainer = document.getElementById("question_container");
+var cardContainer = document.getElementById("card_container");
+var showQuestionsButton = document.getElementById("show_questions")
+
+function showQuestions(){
+    questionContainer.style.display = "flex";
+    cardContainer.style.display = "none";
+}
+
+showQuestionsButton.addEventListener('click', showQuestions);
